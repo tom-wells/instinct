da532ea068d5281667c0e08bdc18a6c864911120
Tom Wells <tom.wells89@gmail.com>
2019-06-18 14:54:55 +0100

    responsive styles with gallery images uploaded

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -6,6 +6,19 @@ videoTag = document.querySelector('.gallery-nav .videography')
 photoTag = document.querySelector('.gallery-nav .photography')
 
 
+navButton = document.querySelector('.mobile-nav-button')
+navMenu = document.querySelector('.mobile-nav')
+navMenuOptions = document.querySelector('.mobile-nav nav')
+
+
+navButton.addEventListener('click', function() {
+    navMenu.classList.toggle('open')
+});
+
+navMenuOptions.addEventListener('click', function() {
+    navMenu.classList.remove('open')
+});
+
 videoTag.addEventListener('click', function() {
     videoTag.classList.add('selected')
     photoTag.classList.remove('selected')
@@ -16,8 +29,34 @@ videoTag.addEventListener('click', function() {
 photoTag.addEventListener('click', function() {
     photoTag.classList.add('selected')
     photoSection.classList.add('open')
+    videoSection.classList.remove('open')
     videoTag.classList.remove('selected')
 
-    videoSection.classList.remove('open')
-
 });
+
+
+$('.images .image').on('click', function() {
+
+
+    var currentImage = $(this).html()
+    $('.modal-content').html(currentImage)
+    $('.modal').fadeIn(500)
+    return false
+})
+
+$('.videos .video').on('click', function() {
+
+
+    var currentImage = $(this).html()
+    $('.modal-content').html(currentImage)
+    $('.modal').fadeIn(500)
+    return false
+})
+
+
+
+
+$('.modal-close, .modal-background').on('click', function() {
+    $('.modal').fadeOut(500)
+    return false
+})
